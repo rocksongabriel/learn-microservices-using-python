6d5640f563564d1bbfb3fac04656da86bb4b2c73
Gabriel Rockson <gabriel.rockson@amalitech.com>
2023-01-04 11:30:53 +0000

    refactor: change the casts to casts_id

--- movie-service/app/api/models.py
+++ movie-service/app/api/models.py
@@ -1,20 +1,23 @@
 from pydantic import BaseModel
 
+
 class MovieBase(BaseModel):
     name: str
     plot: str
     genres: list[str]
-    casts: list[str]
+    casts_id: list[int]
+
 
 class MovieIn(MovieBase):
     pass
 
+
 class MovieOut(MovieBase):
     id: int
 
-    
+
 class MovieUpdate(BaseModel):
     name: str | None = None
     plot: str | None = None
     genres: list[str] | None = None
-    casts: list[str] | None = None
+    casts_id: list[int] | None = None
